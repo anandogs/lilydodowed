1639c53ddd303319381bd93074b5ace2690b496c
Anando <anandoghose@gmail.com>
2021-08-07 11:49:36 +0530

    fixed scroll

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,13 +4,14 @@ import { StaticImage } from "gatsby-plugin-image"
 
 export default function Layout({ children }) {
   return (
+  <div className='centerLayout'>
   <div className="layout">
       <StaticImage src="../images/dl.png"/>
       <div className='inviteHeader'><h1>A little RSVP goes a long way!</h1></div>
       <div className='inviteBody'>
       {children}
       </div>
-      
+      </div>
       
       </div>)
 }
