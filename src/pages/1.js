0cbd7a2d7f52752091abb659438011ccf48ff34b
Anando <anandoghose@gmail.com>
2021-08-07 17:47:03 +0530

    final site.. i think

--- src/pages/1.js
+++ src/pages/1.js
@@ -1,68 +1,19 @@
-import React from "react"
-import { useState } from "react"
-import { Link } from "gatsby"
-import Layout from "../components/Layout"
+import React from 'react'
+import { StaticImage } from 'gatsby-plugin-image'
+import { Link } from 'gatsby'
 import "../styles/global.css"
-import { StaticImage } from "gatsby-plugin-image"
 
-
-export default function Home() {
-  const userStats = {
-    personName: "",
-    personNumber: "",
-    attending11: 0,
-    attending14: 0,
-    plus1: 0,
-    nonVeg: 0,
-  }
-
-  const [values, setValues] = useState(userStats)
-
-  const handleInputChange = e => {
-    const { name, value } = e.target
-    setValues({
-      ...values,
-      [name]: value,
-    })
-  }
-
-  function handleSubmit() {
-    console.log(values)
-  }
-
-  return (
-    <Layout>
-      <div className='invite'>
-      <div className="textBody">
-        <p>Can we have your name and Number please?</p>
-        <form>
-          <input
-            value={values.personName}
-            onChange={handleInputChange}
-            type="text"
-            name="personName"
-            placeholder="Name"
-          />
-          <input
-            value={values.personNumber}
-            onChange={handleInputChange}
-            type="number"
-            name="personNumber"
-            placeholder="Number"
-          />
-          <Link to="/eng" state={values}>
-            <button onClick={handleSubmit}>Submit</button>
-          </Link>
-        </form>
-      </div>
-      <div className='flowerBox'>
-      <StaticImage src='../images/Flower_Highlight_.png' alt='highlighted-flower'/>
-      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
-      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
-      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
-      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
+export default function Landing() {
+    return (
+        
+              <div className='centerLayout'>
+  <div className="layout">
+      <StaticImage className='heroImage' src="../images/dl.png"/>
+      <div className='inviteBody'>
+      <h1>Welcome to our celebrations!</h1>
+        <Link to='/landing883'><button>RSVP!</button></Link>
       </div>
       </div>
-    </Layout>
-  )
+        </div>
+    )
 }
---
+++ src/pages/landing883.js
@@ -0,0 +1,66 @@
+import React from "react"
+import { useState } from "react"
+import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import "../styles/global.css"
+import { StaticImage } from "gatsby-plugin-image"
+
+export default function Home() {
+  const userStats = {
+    personName: "",
+    personNumber: "",
+    attending11: 0,
+    attending14: 0,
+    plus1: 0,
+    nonVeg: 0,
+  }
+
+  const [values, setValues] = useState(userStats)
+
+
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setValues({
+      ...values,
+      [name]: value
+    });
+
+  };
+
+
+  return (
+    <Layout>
+      <div className='invite'>
+      <div className='textBody'>
+      <p>Can we have your name and Number please?</p>
+          <form>
+          <input
+            value={values.personName}
+            onChange={handleInputChange}
+            type="text"
+            name='personName'
+            placeholder="Name"
+          />
+                    <input
+            value={values.personNumber}
+            onChange={handleInputChange}
+            type="number"
+            name='personNumber'
+            placeholder="Number"
+          />
+          <Link to="/rec" state={values}>
+            <button>Submit</button>
+          </Link>
+          </form>
+      </div>
+      <div className='flowerBox'>
+      <StaticImage src='../images/Flower_Highlight_.png' alt='highlighted-flower'/>
+      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
+      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
+      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
+      <StaticImage src='../images/Flower.png' alt='unhighlighted-flower'/>
+      </div>
+      </div>
+    </Layout>
+  )
+}
