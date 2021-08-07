1c6128449cdb066bb3c9581129917fc0fa64e594
Anando <anandoghose@gmail.com>
2021-08-07 11:09:37 +0530

    eeks

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,7 +5,7 @@ import { StaticImage } from 'gatsby-plugin-image'
 export default function NotFound() {
     return (
       <div>
-          <StaticImage src = '../images/eeks.jpeg' alt='unhighlighted-flower'/>
+          <StaticImage src = '../images/eeks.jpg' alt='unhighlighted-flower'/>
       <div style={{textAlign:'center'}}>
         <h2>Erm.. you're not supposed to be here..</h2>
       </div>
